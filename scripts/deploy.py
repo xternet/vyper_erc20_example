91194c951d1dca84f775e11c863d6a763de821e0
xternet <majewski.post@gmail.com>
2021-02-08 12:55:25 +0100

    .

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 from brownie import Token, accounts
 
-acct = accounts.load('1')
+# if deploy on public net
+# acct = accounts.load('1')
 
 def main():
-	acct.deploy(Token, "Token", "TKN", 18, 1000)
+	accounts[0].deploy(Token, "Token", "TKN", 18, 1000)
+
+	# if public on public net
+	# acct.deploy(Token, "Token", "TKN", 18, 1000)
